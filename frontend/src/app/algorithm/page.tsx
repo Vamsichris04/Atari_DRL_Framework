fd239add12474f667053a830dcf1d0fb0770969d
danner-riversg <danner-riversg@msoe.edu>
2026-03-13 11:15:14 -0500

    Adjust algorithm selection page

--- frontend/src/app/algorithm/page.tsx
+++ frontend/src/app/algorithm/page.tsx
@@ -1,7 +1,15 @@
 'use client';
-import { Button } from '@/components/ui/button';
+import { Button } from '@/components/ui/pixelact-ui/button';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/pixelact-ui/card';
 import { useData } from '@/providers/data';
 import { useRouter } from 'next/navigation';
+import '@/components/ui/pixelact-ui/styles/styles.css';
 
 export default function Page() {
   const { setInputValue } = useData();
@@ -9,33 +17,68 @@ export default function Page() {
 
   return (
     <div className="grid gap-4 h-100">
-      <h1 className="text-3xl font-bold text-center">Select an Algorithm</h1>
+      <h1 className="text-3xl font-bold text-center pixel-font">Select an Algorithm</h1>
       <div className="flex justify-center items-center">
-        <div className="grid gap-2">
-          <div className="flex justify-center gap-2">
-            {['DQN', 'Rainbow', 'PPO'].map((algorithm) => (
+        <div className="flex gap-2">
+          <Card className="text-center flex flex-col justify-between">
+            <CardHeader>
+              <CardTitle className="text-xl">DQN</CardTitle>
+            </CardHeader>
+            <CardContent>
+              Stores past experiences and replays them to learn which actions lead to the best
+              rewards.
+            </CardContent>
+            <CardFooter>
               <Button
-                key={algorithm}
                 onClick={() => {
-                  setInputValue('algorithm', algorithm);
+                  setInputValue('algorithm', 'DQN');
                   router.push('/configure');
                 }}
+                className="w-full"
               >
-                {algorithm}
+                Select
               </Button>
-            ))}
-          </div>
-          <p className="text-center">
-            <b>DQN:</b> Stores past experiences and replays them to learn which actions lead to the
-            best rewards.
-          </p>
-          <p className="text-center">
-            <b>Rainbow:</b> An improved version of DQN designed to learn faster and perform better.
-          </p>
-          <p className="text-center">
-            <b>PPO:</b> Learns how to act from recent experiences only, and carefully updates its
-            strategy to stay stable.
-          </p>
+            </CardFooter>
+          </Card>
+          <Card className="text-center flex flex-col justify-between">
+            <CardHeader>
+              <CardTitle className="text-xl">Rainbow</CardTitle>
+            </CardHeader>
+            <CardContent>
+              An improved version of DQN designed to learn faster and perform better.
+            </CardContent>
+            <CardFooter>
+              <Button
+                onClick={() => {
+                  setInputValue('algorithm', 'Rainbow');
+                  router.push('/configure');
+                }}
+                className="w-full"
+              >
+                Select
+              </Button>
+            </CardFooter>
+          </Card>
+          <Card className="text-center flex flex-col justify-between">
+            <CardHeader>
+              <CardTitle className="text-xl">PPO</CardTitle>
+            </CardHeader>
+            <CardContent>
+              Learns how to act from recent experiences only, and carefully updates its strategy to
+              stay stable.
+            </CardContent>
+            <CardFooter>
+              <Button
+                onClick={() => {
+                  setInputValue('algorithm', 'PPO');
+                  router.push('/configure');
+                }}
+                className="w-full"
+              >
+                Select
+              </Button>
+            </CardFooter>
+          </Card>
         </div>
       </div>
     </div>
